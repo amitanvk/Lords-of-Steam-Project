af46c6795ce8327fd99f09b8ecd5fbdf5d2b2309
Dhruv Karunakaran <dhruvn@gmail.com>
2015-01-28 16:06:05 -0500

    Initial Commit

---
+++ MechFightingGame/Assets/Scripts/MechMain.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MechMain : MonoBehaviour {
+
+	public GameObject enemyMech;
+
+	public GUIText healthMeter;
+	public GUIText energyMeter;
+
+	public float health;
+	public float energy;
+	public float healthMax;
+	public float energyMax;
+	public float healthRegen;
+	public float energyRegen;
+
+	public float moveForce;
+	public float hoverForce;
+	public float rotationSpeed;
+
+	public bool lockOn;
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+		energy = 1;
+		lockOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		regen ();
+		control ();
+		state ();
+	}
+
+
+	void regen()
+	{
+
+		if (energy < energyMax) 
+		{
+			energy += energyRegen;
+		}
+		healthMeter.text = "Health: " + health;
+		energyMeter.text = "Energy: " + energy;
+	}
+
+	void control()
+	{
+		Quaternion AddRot = Quaternion.identity;
+		float yaw = 0;
+		if (lockOn) 
+		{
+			transform.LookAt (enemyMech.transform.position);
+		}
+
+		rigidbody.AddForce (Vector3.up * hoverForce);
+
+		if (gameObject.tag == "mech1") 
+		{
+			if(Input.GetButtonDown("lock1"))
+			{
+				lockOn = !lockOn;
+			}
+			rigidbody.AddForce (Input.GetAxis ("Vertical1") * transform.forward * moveForce);
+			if(lockOn)
+			{
+				rigidbody.AddForce (Input.GetAxis ("Horizontal1") * transform.right * moveForce);
+			}
+			else
+			{
+				yaw = Input.GetAxis("Horizontal1") * (Time.fixedDeltaTime * rotationSpeed);
+				AddRot.eulerAngles = new Vector3(0, yaw,0);
+				rigidbody.rotation *= AddRot;
+			}
+		}
+		if (gameObject.tag == "mech2") 
+		{
+			if(Input.GetButtonDown("lock2"))
+			{
+				lockOn = !lockOn;
+			}
+			rigidbody.AddForce (Input.GetAxis ("Vertical2") * -1 * transform.forward * moveForce);
+			if(lockOn)
+			{
+				rigidbody.AddForce (Input.GetAxis ("Horizontal2") * transform.right * moveForce);
+			}
+			else
+			{
+				yaw = Input.GetAxis("Horizontal2") * (Time.fixedDeltaTime * rotationSpeed);
+				AddRot.eulerAngles = new Vector3(0, yaw,0);
+				rigidbody.rotation *= AddRot;
+			}
+		}
+
+	}
+
+	void attack()
+	{
+
+
+	}
+
+	void state()
+	{
+		if (health <= 0) {
+			Destroy(gameObject);
+				}
+
+	}
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "firetrap") 
+		{
+			health -= 0.25f;
+		}
+	}
+}
---
+++ MechFightingGame/Assets/testShotgun.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testShotgun : MonoBehaviour {
+
+	public bool active;
+	public GameObject parentObject;
+	ParticleSystem particle;
+	// Use this for initialization
+	void Start () {
+		active = false;
+		particle = GetComponentInParent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (parentObject.tag == "mech1") 
+		{
+
+			if (Input.GetButtonDown ("Fire11") && !active && GetComponentInParent<MechMain>().energy > 10) {
+				particle.Emit (10);
+				GetComponentInParent<MechMain>().energy -=10;
+				StartCoroutine (fire ());
+			}
+		}
+		if (parentObject.tag == "mech2") 
+		{
+			
+			if (Input.GetButtonDown ("Fire12") && !active && GetComponentInParent<MechMain>().energy > 10) {
+				particle.Emit (10);
+				GetComponentInParent<MechMain>().energy -=10;
+				StartCoroutine (fire ());
+			}
+		}
+	}
+	IEnumerator fire()
+	{
+		active = true;
+		yield return new WaitForSeconds(0.15f);
+		active = false;
+
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (active && (other.tag == "mech2")) {
+			Debug.Log("mech 2 hit");
+			active = false;
+			other.GetComponentInParent<MechMain>().health -= 10;
+
+				}
+		if (active && (other.tag == "mech1")) {
+			Debug.Log("mech1 hit");
+			active = false;
+			other.GetComponentInParent<MechMain>().health -= 10;
+			
+		}
+	}
+}
