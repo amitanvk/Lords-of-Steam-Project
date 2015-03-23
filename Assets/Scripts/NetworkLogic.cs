08f3e2e46206ffdee363e90f9c58dcd2a3e44fe0
Christian Zajac <Christian Zajac@TheBeast.attlocal.net>
2015-03-23 13:01:11 -0400

    Some Basic Network work accomplished

--- Assets/Scripts/NetworkLogic.cs
+++ Assets/Scripts/NetworkLogic.cs
@@ -3,20 +3,41 @@ using System.Collections;
 
 public class NetworkLogic : MonoBehaviour {
 
-	// Use this for initialization
+	public Camera standbyCamera;
+	public SpawnSpot[] spawnSpots;
+
 	void Start () {
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
 		Connect ();
 	}
 	
 	// Update is called once per frame
 	void Connect (){
 		PhotonNetwork.ConnectUsingSettings("Lords-Of-Steam-v1.0");
+
 	}
-	void OnGui(){
+	void OnGUI(){
 		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
 	}
 	void OnJoinedLobby(){
 		Debug.Log ("?!?!?!");
 		PhotonNetwork.JoinRandomRoom ();
+	}
+	void OnJoinedRoom(){
+		Debug.Log ("OnJoinedRoom");
+		SpawnMyPlayer();
+	}
+	void OnPhotonRandomJoinFailed() {
+		Debug.Log ("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom( null );
+	}
+	void SpawnMyPlayer(){
+		SpawnSpot mySpawnSpot = spawnSpots [Random.Range (0, spawnSpots.Length)];
+		GameObject myPlayer = (GameObject)PhotonNetwork.Instantiate("MechNetwork", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation,0);
+		standbyCamera.enabled = false;
+		myPlayer.GetComponent<MechMain>().enabled = true;
+		//myPlayer.GetComponent<NetworkCharacter>().enabled = true;
+		myPlayer.transform.FindChild ("Main Camera").gameObject.SetActive (true);
+
 	}
 }
