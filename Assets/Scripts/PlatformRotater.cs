4e21dd4dd0ad0f643baf592a3866183a57794b15
Dhruv Karunakaran <dhruvn@gmail.com>
2015-03-25 20:16:40 -0400

    Platform / sky arena start

---
+++ Assets/Scripts/PlatformRotater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformRotater : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vector = new Vector3 (0, 1, 0);
+		transform.Rotate (vector, speed);
+	}
+}
