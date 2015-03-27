785f3f6bf626df33b441effe227eb38e685af28d
Dhruv Karunakaran <dhruvn@gmail.com>
2015-03-27 10:41:12 -0400

    Finished level geometry sky arena

---
+++ Assets/PlatformOrbiter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformOrbiter : MonoBehaviour {
+
+	public float angle;
+	public Transform point;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vector = new Vector3 (0, 1, 0);
+		transform.RotateAround (point.position, vector, angle);
+	}
+}
